073ee58d6b6a79e4eefba410f2b3e62d511550b3
KevinRANDRIANARISON <92078739+KevinRANDRIANARISON@users.noreply.github.com>
2021-10-07 13:34:28 +0400

    Update exemple.php

--- exemple.php
+++ exemple.php
@@ -1,6 +1,31 @@
 <?php
-	$message = 'hello';
+
+	//commentaire sur une ligne
+/*
+exemple de variable
+*/
+	$message = 'hello'; 
 	$message2 = 'world';
 
-	echo "$message". ' ' ."$message2";
+	echo $message. ' ' .$message2."<br>";
+
+//exemple des condition (if)
+	$age = 18;
+	if ($age < 18 ) {
+		echo "pas bon";// la condition
+	}else {
+		echo 'yessss<br>';// code...
+	}
+
+//condition elseif
+
+	if ($age < 18 ) {
+		echo "pas bon";// la condition
+	}elseif ($age == 18) {
+		echo "bravo<br>";// code...
+	}
+	else {
+		echo 'yessss<br>';// code...
+	}
+
 ?>
